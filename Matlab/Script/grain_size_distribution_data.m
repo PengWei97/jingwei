11935e25fee56a5cb4e0e5e99c54b606181b3f1a
Wei Peng <1248110286@qq.com>
2021-04-28 09:53:15 +0800

    1

--- Matlab/Script/grain_size_distribution_data.m
+++ Matlab/Script/grain_size_distribution_data.m
@@ -7,6 +7,7 @@
 %% 目的
 % 已知数据集合（x,y）
 % 计算晶粒尺寸分布曲线
+% 需要简化，使用for语言
 
 close all
 
@@ -81,62 +82,66 @@ for i = 2:num_x(num)
 end
 num_rate_biGG(num,1) = num_rate_biGG(num,1)/(num_x(num)-1)
 
-% 4
-num = 4
-num_data = 10;
-plot(a(2:num_x(4),num_data)-a(1,num_data),a(2:num_x(4),num_data+1)-a(1,num_data+1),...
-    'color','c',...
-    'Marker','p',...
-    'MarkerFaceColor','c',...
-    'MarkerSize',num_MarkerSize,...
-    'LineWidth',num_LineWidth,...
-    'DisplayName',data_DisplayName(4,:));
+% % 4
+% num = 4
+% num_data = 10;
+% plot(a(2:num_x(4),num_data)-a(1,num_data),a(2:num_x(4),num_data+1)-a(1,num_data+1),...
+%     'color','c',...
+%     'Marker','p',...
+%     'MarkerFaceColor','c',...
+%     'MarkerSize',num_MarkerSize,...
+%     'LineWidth',num_LineWidth,...
+%     'DisplayName',data_DisplayName(4,:));
     
-for i = 2:num_x(num)
-    num_rate_biGG(num,1) = num_rate_biGG(num,1) + ((a(i,num_data+1)/3.14)^0.5-(a(1,num_data+1)/3.14)^0.5)/(a(i,num_data)-a(1,num_data));
-end
-num_rate_biGG(num,1) = num_rate_biGG(num,1)/(num_x(num)-1)
+% for i = 2:num_x(num)
+%     num_rate_biGG(num,1) = num_rate_biGG(num,1) + ((a(i,num_data+1)/3.14)^0.5-(a(1,num_data+1)/3.14)^0.5)/(a(i,num_data)-a(1,num_data));
+% end
+% num_rate_biGG(num,1) = num_rate_biGG(num,1)/(num_x(num)-1)
 
-% 5
-num = 5
-num_data = 13;
-plot(a(2:num_x(5),num_data)-a(1,num_data),a(2:num_x(5),num_data+1)-a(1,num_data+1),...
-    'color','m',...
-    'Marker','d',...
-    'MarkerFaceColor','m',...
-    'MarkerSize',num_MarkerSize,...
-    'LineWidth',num_LineWidth,...
-    'DisplayName',data_DisplayName(5,:));
+% % 5
+% num = 5
+% num_data = 13;
+% plot(a(2:num_x(5),num_data)-a(1,num_data),a(2:num_x(5),num_data+1)-a(1,num_data+1),...
+%     'color','m',...
+%     'Marker','d',...
+%     'MarkerFaceColor','m',...
+%     'MarkerSize',num_MarkerSize,...
+%     'LineWidth',num_LineWidth,...
+%     'DisplayName',data_DisplayName(5,:));
     
-for i = 2:num_x(num)
-    num_rate_biGG(num,1) = num_rate_biGG(num,1) + ((a(i,num_data+1)/3.14)^0.5-(a(1,num_data+1)/3.14)^0.5)/(a(i,num_data)-a(1,num_data));
-end
-num_rate_biGG(num,1) = num_rate_biGG(num,1)/(num_x(num)-1)
+% for i = 2:num_x(num)
+%     num_rate_biGG(num,1) = num_rate_biGG(num,1) + ((a(i,num_data+1)/3.14)^0.5-(a(1,num_data+1)/3.14)^0.5)/(a(i,num_data)-a(1,num_data));
+% end
+% num_rate_biGG(num,1) = num_rate_biGG(num,1)/(num_x(num)-1)
 
-% % ylim([6200 12000])
-% % xlim([0,6000])
-num_label_FontSize = 8;
-xlabel('t-t1/(ns)',...
-  'FontSize',num_label_FontSize,...
+num_FontSize_label = 8
+num_FontSize_legend = 10
+
+xlabel('R/<R>',...
+  'FontSize',num_FontSize_label,...
   'FontWeight','bold',...
   'Color','k')
-ylabel('A(t)-A(t1)/(nm^2)',...
-      'FontSize',num_label_FontSize,...
+ylabel('Related Frequency',...
+      'FontSize',num_FontSize_label,...
       'FontWeight','bold',...
       'Color','k')
-set(gca,'FontSize',num_label_FontSize,'Fontwei','Bold','Linewidth',1);
-set(gca,'YTick',[-150000:30000:0]) %改变x轴坐标间隔显示 这里间隔为2
-lgd = legend('FontSize',5,'TextColor','black','Location','southwest');
-% Location:northeast,northwest,southeast,southwest
+set(gca,'FontSize',num_FontSize_legend,'Fontwei','Bold','Linewidth',1)
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%
+% lgd = legend({'(a) 100.0ns','(b) 1000ns','(c) 3000ns','(d) 5000ns'},...
+%             'FontSize',num_FontSize_legend,'TextColor','black','Location','northeast');
+
+lgd = legend('FontSize',num_FontSize_legend,'TextColor','black','Location','northeast');
+
+
+ylim([0 1.4])
+xlim([0,2.5])
 
 hfig = figure(1);
-figWidth = 6.5;
-figHight = 6.3;
+figWidth = 10;
+figHight = 6.5;
 % 7.3,7 for fcc_word
 
 set(hfig,'PaperUnits','centimeters');
 set(hfig,'PaperPosition',[0 0 figWidth figHight])
-fileout = [mat2str(10)];
+fileout = [mat2str(12)];
 print(hfig,[fileout,'bi_0_9'],'-r300','-dpng')
